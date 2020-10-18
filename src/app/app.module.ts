471be154e1fb87960144c108b6f17ec50ab26f3b
rustamsafarovrs <rs.programmer@hotmail.com>
2020-10-18 10:06:56 +0500

    added filter

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,29 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {TodosComponent} from './todos/todos.component';
+import { HttpClientModule} from '@angular/common/http';
+import { TodoFormComponent } from './todo-form/todo-form.component';
+import {FormsModule} from '@angular/forms';
+import {TodosFilterPipe} from './shared/todos-filter.pipe';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TodosComponent,
+    TodoFormComponent,
+    TodosFilterPipe
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
---
+++ src/app/todos/todos.component.ts
@@ -0,0 +1,33 @@
+import {Component, OnInit} from '@angular/core';
+import {TodosService} from '../shared/ todos.service';
+import {delay} from 'rxjs/operators';
+
+@Component({
+  selector: 'app-todos',
+  templateUrl: './todos.component.html',
+  styleUrls: ['./todos.component.css'],
+})
+export class TodosComponent implements OnInit {
+
+  private search: string = '';
+  loading: boolean = true;
+
+  constructor(public todosService: TodosService) {
+  }
+
+  ngOnInit(): void {
+    this.todosService.fetchTodos()
+      .pipe(delay(500))
+      .subscribe(() => {
+      this.loading = false;
+    });
+  }
+
+  onChange(id: number) {
+    this.todosService.onToggle(id);
+  }
+
+  removeTodo(id: number) {
+    this.todosService.removeTodo(id);
+  }
+}
